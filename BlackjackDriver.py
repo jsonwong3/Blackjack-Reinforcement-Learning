48964f7946a568733d4d2c438d33488e2e4eeb39
Jason Wong <42827027+iceclimber3@users.noreply.github.com>
2019-03-29 21:10:51 -0400

    Add files via upload

--- BlackjackDriver.py
+++ BlackjackDriver.py
@@ -3,13 +3,12 @@ from terminaltables import AsciiTable
 from os import system, name
 import time
 
-import Deck, Hand
 import BlackjackPlayer
 
-# GETTING 21 DOESNT YIELD 1.5X
 # BUGS WIRH ACE IN HAND, BUSTING WHEN IT SHOULDNT
-# UI PROBLEMS
-    
+# Merge files together into one, create diff classes
+# change code so that players can only have 1 hand
+
 # Clears user interface
 def clear():
     # For windows
@@ -18,6 +17,112 @@ def clear():
     # For Mac and Linux
     else:
         _ = system('clear')
+
+# Standard 52-Card Deck
+class Deck:
+    suits = ['Diamonds', 'Hearts', 'Spades', 'Clubs']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+              'Jack', 'Queen', 'King', 'Ace']
+    
+    # Constructor for a deck of the given size
+    def __init__(self, size: int=1):
+        self._deck = []
+        self._suits = ['Diamonds', 'Hearts', 'Spades', 'Clubs']
+        self._values = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+                        'Jack', 'Queen', 'King', 'Ace']        
+        for i in range(0, size):
+            # Add the requested amount of cards into the list
+            for value in self._values:
+                for suit in self._suits:
+                    self._deck.append((value + ' of ' + suit))
+        self.sort()
+        
+    # Return playing card deck
+    def get_deck(self) -> list:
+        return self._deck
+    
+    # Pop the first card in the deck
+    def draw(self) -> str:
+        if self.size != 0:
+            return self._deck.pop(0)
+
+    # Remove card from deck
+    def remove(self, value: str, suit: str=None):
+        # If the suit is not given, remove the first card which
+        # holds the given numberic value
+        # If the suit is given, remove the first card which holds
+        # both the given numberic value and suit value
+        if suit is None:
+            removed = False
+            i = 0
+            while (not(removed) and i < self.size()):
+                if self._deck[i].startswith(value):
+                    self._deck = self._deck[:i] + self._deck[i+1:]
+                    removed = True
+                else:
+                    i += 1
+        else:
+            self._deck.remove((value + ' of ' + suit))
+
+   # Shuffle the deck
+    def shuffle_deck(self):
+        shuffle(self._deck)
+
+    # Return size of deck
+    def size(self) -> int:
+        return len(self._deck)
+    
+    # Sort the deck via bubble sort
+    def sort(self):
+        self._values = {'1': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+
+        # Use bubble sort to sort deck
+        for i in range(len(self._deck)-1,0,-1):
+            for j in range(i):
+                # Find numberic value of each card
+                if self._deck[j][0] in self._values:
+                    first = self._values[self._deck[j][0]]
+                else:
+                    first = int(self._deck[j][0])
+
+                if self._deck[j+1][0] in self._values:
+                    second = self._values[self._deck[j+1][0]]
+                else:
+                    second = int(self._deck[j+1][0])
+
+                # Swap positions of cards if the second card
+                # is less than the first
+                if first > second:
+                    temp = self._deck[j]
+                    self._deck[j] = self._deck[j+1]
+                    self._deck[j+1] = temp
+
+# Blackjack Hand
+class Hand:
+    # Constructor for a Blackjack hand
+    def __init__(self, firstCard: str, secondCard: str):
+        self._cards = [firstCard, secondCard]
+        self._bet = 0
+
+    # Return the bet associated with the hand
+    def get_bet(self) -> float:
+        return self._bet
+
+    # Set bet assocaited with the hand
+    def set_bet(self, bet: float):
+        self._bet = bet
+
+    # Return cards in hand
+    def get_cards(self) -> list:
+        return self._cards
+
+    # Set cards in hand
+    def set_cards(self, firstCard: str, secondCard: str):
+        self._cards = [firstCard, secondCard]
+
+    # Add card to hand
+    def add(self, card: str):
+        self._cards.append(card)
 
 # Check if the string is a valid float value
 def is_float(string: str) -> bool:
@@ -50,10 +155,13 @@ class BlackjackDriver():
         self._dealer = 'Dealer Object'
         self._table = []
         self._numOfBot = 0
+        self._command_list = []
+        self._round_num = 0
         
         # Player variables
         self._player = 'Player Object'
         self._buyIn = 500.00
+        endturn = False
         
         # Deck variables
         self._deck = 'Deck Object'
@@ -115,23 +223,31 @@ class BlackjackDriver():
 
                     while not(self.all_players_end()):
                         clear()
+                        self._round_num += 1
+                        self._command_list.append("\n --- Round " + str(self._round_num) + " ---")
                         self.display_hands()
                         for unit in self._table:
                             if not(unit.is_end_turn()):
-                                time.sleep(2)
+                                time.sleep(1.5)
                                 if unit is self._player:
-                                    # Prompt user for in-game action
-                                    action = input('\n1) Stand' + 
-                                                    '\n2) Hit' +  
-                                                    '\n3) Clear Screen\n')
+                                    endturn = False
+                                    while not(endturn):
+                                        # Prompt user for in-game action
+                                        action = input('\n1) Stand' + 
+                                                        '\n2) Hit' +  
+                                                        '\n3) Clear Screen\n\n')
                                 else:
                                     action = self.execute_bot_command(unit)
                                 if action == '1':
                                     self.stand(unit)
+                                    endturn = True
                                 elif action == '2':
                                     self.hit(unit)
+                                    endturn = True
                                 elif action == '3':
                                     clear()
+                            self.display_hands()
+                        time.sleep(1)
                     self.showdown() 
                     continue_prompt = input("\nPress any button to start next round")
             # Options Menu
@@ -211,17 +327,20 @@ class BlackjackDriver():
         for unit in self._table:
             unit.clear_hands()
             unit.clear_turn()
-            
+        self._dealer.clear_hands()
+        self._command_list = []
+        self._round_num = 0
+
     # Create a new shuffled deck
     def new_deck(self):
         # Set proper amount of decks being used
-        self._deck = Deck.Deck(self._deckSize)
+        self._deck = Deck(self._deckSize)
         self._maxCards = self._deckSize * 52
         self._deck.shuffle_deck()
     
     # Deal 2 cards to the unit provided
     def deal(self, unit, bet: float=0):
-        tempHand = Hand.Hand(self._deck.draw(), self._deck.draw())
+        tempHand = Hand(self._deck.draw(), self._deck.draw())
         tempHand.set_bet(bet)
         unit.add_hand(tempHand)
     
@@ -257,20 +376,23 @@ class BlackjackDriver():
         
         table_instance = AsciiTable(table_data, title)
         table_instance.justify_columns[1] = 'right'
+        
+        clear()
         print()
         print(table_instance.table)
-      
+        for text in self._command_list:
+            print(text)
+            
     # End players turn
     def stand(self, player):
-        print("\n" + player.get_name() + " stands")
+        self._command_list.append("\n" + player.get_name() + " stands")
         player.end_turn()
 
     # Draw player another card
     def hit(self, player, i: int=0):
-        print("\n" + player.get_name() + " hits")
+        self._command_list.append("\n" + player.get_name() + " hits")
         player.get_hands()[i].add(self._deck.draw())
         self.isBust(player)
-        self.display_hands()
 
     # Check if every player finished their turn
     def all_players_end(self) -> bool:
@@ -319,6 +441,8 @@ class BlackjackDriver():
     def exexute_dealer_command(self, dealer):
         while(self.calculate_hand(dealer.get_hands()[0]) < 17):
             self.hit(dealer)
+            self.display_hands()
+            time.sleep(1.5)
 
     # Check if every player busted
     def all_players_bust(self) -> bool:
@@ -337,19 +461,22 @@ class BlackjackDriver():
                 all_busted = False
                 showdown_list.append(unit)
             else:
-                print("\n" + unit.get_name() + " busted")
+                self._command_list.append("\n" + unit.get_name() + " busted")
                 time.sleep(1.5)
                 unit.set_cash(unit.get_cash() - unit.get_hands()[0].get_bet())
         if not(all_busted):
-            print("\nStarting Showdown")
+            self._command_list.append("\n --- Starting Showdown ---")
             time.sleep(1.5)
             self.display_hands()
             self.exexute_dealer_command(self._dealer)
             if (self._dealer.is_busted()):
-                print("\nDealer busted")
+                print("\nDealer busted, everyone in the showdown wins")
                 time.sleep(1.5)            
                 for unit in showdown_list:
-                    unit.set_cash(unit.get_cash() + unit.get_hands()[0].get_bet())
+                    bet = unit.get_hands()[0].get_bet()
+                    unit_hand_value = self.calculate_hand(unit.get_hands()[0])
+                    reward = bet * 1.5 if unit_hand_value == 21 else bet
+                    unit.set_cash(unit.get_cash() + reward)
             else:
                 dealer_hand_value = self.calculate_hand(self._dealer.get_hands()[0])
                 for unit in showdown_list:
@@ -362,22 +489,25 @@ class BlackjackDriver():
                         print("\n" + unit.get_name() + " lost against the dealer")
                         time.sleep(1.5)
                     elif dealer_hand_value < unit_hand_value:
-                        unit.set_cash(unit.get_cash() + unit.get_hands()[0].get_bet())
+                        bet = unit.get_hands()[0].get_bet()
+                        reward = bet * 1.5 if unit_hand_value == 21 else bet
+                        unit.set_cash(unit.get_cash() + reward)
                         print("\n" + unit.get_name() + " won against the dealer")
                         time.sleep(1.5)
 
-if (__name__ == "__main__"):
-    isRunning = True
     
-    while(isRunning):
-        program = input('Select program:\n' + 
-                        '1) Blackjack\n' + 
-                        '2) Exit Program\n')
-        if program == '1':
-            print('\nStarting BlackJack Game')
-            p = BlackjackDriver()
-        elif program == '2':
-            print('\nExiting Program')
-            exit()
-        else:
-            pass
+if (__name__ == "__main__"):
+    BlackjackDriver()
+    #isRunning = True
+    #while(isRunning):
+        #program = input('Select program:\n' + 
+         #               '1) Blackjack\n' + 
+          #              '2) Exit Program\n')
+        #if program == '1':
+         #   print('\nStarting BlackJack Game')
+          #  p = BlackjackDriver()
+        #elif program == '2':
+         #   print('\nExiting Program')
+          #  exit()
+        #else:
+         #   pass
